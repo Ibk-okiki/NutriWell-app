f93aaa50c007d28853216859ebebf907b399d905
Okiki# <ibukunoluwaadelokiki@gmail.com>
2025-09-08 04:11:48 +0100

    Extended tailwind.config.js with brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        nutri: {
+          green: "#2F855A",   // Figma green
+          orange: "#DD6B20",  // Figma orange
+          dark: "#2E4A3F",    // Text color
+        },
+      },
+    },
   },
   plugins: [],
 }
